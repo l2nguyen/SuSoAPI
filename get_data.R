a2f254699aa1b47df5e0b101613b33ab56141844
l2nguyen <lena.nguyen4@gmail.com>
2018-06-26 17:37:37 -0400

    Initial commit of new download data from one questionnaire function

---
+++ get_data.R
@@ -0,0 +1,160 @@
+#---------------------------------------------#
+#------- EXPORT DATA FROM ONE TEMPLATE -------#
+#---------------------------------------------#
+# Args: 
+# server: server prefix
+# user: API user ID, default is APIuser
+# password: password for API user, default is Password123
+# qx_name: Name of the questionnaire to download
+# version: version number of the data you would like to export
+# export type: the data type that you would like to export
+# options are tabular, stata, spss, binary, paradata
+# folder: the directory you would like to export the data into. Use '\\' instead of '\'
+#
+# Returns:
+# The exported data will be downloaded into the specified directory
+# It also unzips the downloaded file into the same directory
+
+dl_data <- function(server,  # server prefix
+                    user = "APIuser",  # API user ID
+                    password = "Password123",  # password
+                    qx_name,  # Name of questionnaire (not template ID)
+                    version = 1,  # version number
+                    export_type ="tabular", # export type
+                    folder)
+{
+  source("check_setup.R")
+  source("get_details.R")
+  source("get_qx.R")
+  source("get_qx_id.R")
+  
+  # build base URL for API
+  api_URL <- sprintf("https://%s.mysurvey.solutions/api/v1", 
+                     server)
+  
+  check_setup(api_URL, login, password)
+  
+  # Get ID of template to get export URL
+  template <- get_qx_id(server, qx_name, user, password)
+  
+  export_URL <- sprintf("%s/export/%s/%s$%i/", 
+                        api_URL, export_type, template, version)
+  
+  # -----------------------------------------------------------------------------
+  # Request export files to be created
+  # -----------------------------------------------------------------------------
+  # post request to API
+  start_query = paste0(export_URL, "start")
+  
+  startExport <- POST(start_query, authenticate(login, password))
+  
+  # get result of query
+  result <- status_code(data)
+  # Get start time of export
+  start_time <- as.POSIXct(headers(startExport)$date, format = "%a, %d %b %Y %H:%M:%S", tz = "GMT")
+  
+  # convert start time into UTC for standardization with server response time
+  start_time <- with_tz(start_time, "UTC")
+  
+  # -----------------------------------------------------------------------------
+  # Obtain status of export file creation
+  # -----------------------------------------------------------------------------
+  
+  if (status_code(startExport) == 200) {   #if request was posted sucessfully
+    # Display message that the exporting process is starting
+    message("Requesting data sets to be compiled on server...")
+    
+    # wait 1 second between start request and status request
+    Sys.sleep(1)
+    
+    #-- Get details of export status --#
+    get_details(export_URL, login, password)
+    
+  }	else if (status_code(startExport) == 400 | status_code(startExport) == 404) {
+    stop_for_status(start_result,
+                    "to find questionnaire. Check template name and version number.")
+  }
+  
+  # Wait 30 seconds for export to be made
+  message("Waiting for export files to be generated...")
+  Sys.sleep(10)
+  
+  # -----------------------------------------------------------------------------
+  # React to status
+  # -----------------------------------------------------------------------------
+  
+  # If server is still working on generating export data, wait and then check status again
+  while (export_details$ExportStatus %in% c("NotStarted", "Queued", "Running")) {
+    # Wait 30 seconds
+    Sys.sleep(10)
+    
+    # Check details again
+    get_details(export_URL, login, password)
+  
+    # If running or queued, keep waiting and check status again
+    if (export_details$ExportStatus %in% c("Queued","Running")) {
+      Sys.sleep(5)
+    }
+    
+    # if creation of files not started
+    if (export_details$ExportStatus == "NotStarted" & export_details$HasExportedFile == TRUE) {
+      
+      # check if exported file has already finished and export file now exists
+      # NOTE: Tabular data files generate so quickly that the server has reverted
+      # back to "Not Started" status by the time we check for details.
+      
+      # if last update is after request, then file is ready to download
+      if (is.null(last_update) == TRUE | last_update >= start_time) {
+        
+        # Change export status to finished because it is finished
+        export_details$ExportStatus <- "Finished"
+        
+        # exit while loop, and download file
+        break
+        
+      } else if (last_update < startReqTime) {
+        # start export again if query did not go through for some reason
+        startExport <- POST(start_query, authenticate(login, password))
+      }
+    }
+  }
+  
+  # If export is file is finished being produced, download data file
+  if (details$ExportStatus == "Finished") {
+    # Set folder to directory specified by user
+    # concatenate file name
+    zip_path <- paste0(folder,"\\", 
+                       qx_name, "_", 
+                       "v", version, "_", 
+                       export_type, "_", 
+                       format(Sys.time(), "%d_%b_%Y"))
+    
+    # name of zip file
+    zip_name <- paste0(zip_path,".zip")
+    
+    # Query to download data
+    downloadData <- GET(
+      export_URL,
+      authenticate(login, password)
+    )
+    
+    if (status_code(downloadData) == 200) {
+      
+      bin <- content(downloadData,"raw")
+      # write content to the zip file
+      writeBin(bin, zip_name)
+      message("Sucessfully exported ", qx_name, " version ", version)
+      
+      unzip(zip_name,exdir = zip_path)
+      message("Data files successfully downloaded into folder: ", "\n", zip_path)
+    } else {
+      failureDesc	= paste0("Problem dowloading. Server status code: ", status_code(downloadData))
+    }
+    
+  } 
+  
+  # export with errors - work in progress
+  # if (details$ExportStatus == "FinishedWithErrors") {
+  #   # add something for it to try again
+  # }
+}
