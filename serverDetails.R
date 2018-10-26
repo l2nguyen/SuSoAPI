ebc44bf3ee2cb2de36681bc6eba21823fc18aa1d
Lena Nguyen <lena.nguyen4@gmail.com>
2018-10-26 16:54:41 -0400

    Added back server details template

---
+++ serverDetails.R
@@ -0,0 +1,27 @@
+# =============================================================================
+# Sever details
+# =============================================================================
+# NOTE: Fill in this template with details for you own server before using.
+
+# details for accessing server
+server    <- ""		    # first part of server address
+login 		<- ""		    # login of admin or API user
+password 	<- ""			# password of that user
+
+# data type - only fill this in if you are downloading one type of data a lot
+dataType <- "stata"			# possible (relevant) values (not case sensitive):
+								# tabular		tab-delimited data
+								# stata			Stata 14 data
+								# spss			SPSS data
+								# binary		image and sound files
+                # paradata
+
+# =============================================================================
+# Client details
+# =============================================================================
+
+# where data will be downloaded to
+dataDir <- ""
+							# use forward slash /  in file path
+							# instead of backlash \ , typical for Windows
+							# be sure to put a / at the end of the file path
